6a67ef982545b8c268f9cac79a86e834dfd8aa48
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2025-01-10 14:40:00 +0100

    Change Tailwind config from .js to .ts

--- apps/vendor/tailwind.config.ts
+++ apps/vendor/tailwind.config.ts
@@ -1,5 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+import tailwindcss_animate from "tailwindcss-animate";
+
+export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
@@ -60,5 +63,5 @@ module.exports = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
-}
+  plugins: [tailwindcss_animate]
+} satisfies Config
