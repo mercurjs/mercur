af4fac77841df594333bcb188074858a63cfc4fb
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-01-13 09:18:57 +0100

    add middlewares

--- apps/backend/src/api/vendor/middlewares.ts
+++ apps/backend/src/api/vendor/middlewares.ts
@@ -6,10 +6,11 @@ import { vendorCors } from './cors'
 import { vendorFulfillmentSetsMiddlewares } from './fulfillment-sets/middlewares'
 import { vendorInventoryItemsMiddlewares } from './inventory-items/middlewares'
 import { vendorInvitesMiddlewares } from './invites/middlewares'
+import { vendorOrderMiddlewares } from './orders/middlewares'
 import { vendorPayoutAccountMiddlewares } from './payout-account/middlewares'
 import { vendorPayoutMiddlewares } from './payouts/middlewares'
-import { vendorOrderMiddlewares } from './orders/middlewares'
 import { vendorProductsMiddlewares } from './products/middlewares'
+import { vendorReturnRequestsMiddlewares } from './return-request/middlewares'
 import { vendorSellersMiddlewares } from './sellers/middlewares'
 import { vendorShippingOptionsMiddlewares } from './shipping-options/middlewares'
 import { vendorStockLocationsMiddlewares } from './stock-locations/middlewares'
@@ -59,5 +60,5 @@ export const vendorMiddlewares: MiddlewareRoute[] = [
   ...vendorInventoryItemsMiddlewares,
   ...vendorPayoutMiddlewares,
   ...vendorOrderMiddlewares,
-  ...vendorInventoryItemsMiddlewares
+  ...vendorReturnRequestsMiddlewares
 ]
