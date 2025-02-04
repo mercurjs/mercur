c596e6d9b2edc5cb62e43c15dc4ca51037112ab9
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-02-04 12:13:42 +0100

    fix: authentication middleware

--- apps/backend/src/api/store/middlewares.ts
+++ apps/backend/src/api/store/middlewares.ts
@@ -1,10 +1,14 @@
-import { MiddlewareRoute } from '@medusajs/framework'
+import { MiddlewareRoute, authenticate } from '@medusajs/framework'
 
 import { storeCartsMiddlewares } from './carts/middlewares'
 import { storeOrderReturnRequestsMiddlewares } from './return-request/middlewares'
 import { storeReviewMiddlewares } from './reviews/middlewares'
 
 export const storeMiddlewares: MiddlewareRoute[] = [
+  {
+    matcher: '/store/*',
+    middlewares: [authenticate('customer', ['bearer', 'session'])]
+  },
   ...storeCartsMiddlewares,
   ...storeOrderReturnRequestsMiddlewares,
   ...storeReviewMiddlewares
