02be6582e2bec1309c83a555ef898d9975b224a1
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-02-12 07:05:35 +0100

    add seller request decision emails

---
+++ apps/backend/src/modules/requests/types/events.ts
@@ -0,0 +1,19 @@
+export enum SellerRequestUpdatedEvent {
+  ACCEPTED = 'requests.seller.accepted',
+  REJECTED = 'requests.seller.rejected'
+}
+
+export enum ProductCategoryRequestUpdatedEvent {
+  ACCEPTED = 'requests.product_category.accepted',
+  REJECTED = 'requests.product_category.rejected'
+}
+
+export enum ProductCollectionRequestUpdatedEvent {
+  ACCEPTED = 'requests.product_collection.accepted',
+  REJECTED = 'requests.product_collection.rejected'
+}
+
+export enum ProductRequestUpdatedEvent {
+  ACCEPTED = 'requests.product.accepted',
+  REJECTED = 'requests.product.rejected'
+}
