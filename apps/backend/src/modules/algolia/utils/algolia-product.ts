534ccb20764815f7bf091ecb70543933d6c2f3dd
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-02-20 12:43:21 +0100

    update product structure

---
+++ apps/backend/src/modules/algolia/utils/algolia-product.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod'
+
+import { MedusaContainer } from '@medusajs/framework'
+import { ContainerRegistrationKeys } from '@medusajs/framework/utils'
+
+import { AlgoliaProductValidator } from '../types'
+
+export async function findAndTransformAlgoliaProducts(
+  container: MedusaContainer,
+  ids: string[] = []
+) {
+  const query = container.resolve(ContainerRegistrationKeys.QUERY)
+  const { data: products } = await query.graph({
+    entity: 'product',
+    fields: [
+      '*',
+      'categories.name',
+      'collection.title ',
+      'tags.value',
+      'type.value',
+      'variants.*',
+      'brand.name',
+      'options.*',
+      'options.values.*'
+    ],
+    filters: ids.length
+      ? {
+          id: ids
+        }
+      : {}
+  })
+
+  for (const product of products) {
+    product.options = product.options
+      ?.map((option) => {
+        return option.values.map((value) => {
+          const entry = {}
+          entry[option.title.toLowerCase()] = value.value
+          return entry
+        })
+      })
+      .flat()
+  }
+
+  return z.array(AlgoliaProductValidator).parse(products)
+}
--- apps/backend/src/subscribers/products-changed.ts
+++ apps/backend/src/subscribers/products-changed.ts
@@ -1,40 +1,20 @@
-import { z } from 'zod'
-
 import { SubscriberArgs, SubscriberConfig } from '@medusajs/framework'
-import { ContainerRegistrationKeys } from '@medusajs/framework/utils'
 
 import { ALGOLIA_MODULE } from '../modules/algolia'
 import AlgoliaModuleService from '../modules/algolia/service'
-import {
-  AlgoliaEvents,
-  AlgoliaProductValidator,
-  IndexType
-} from '../modules/algolia/types'
+import { AlgoliaEvents, IndexType } from '../modules/algolia/types'
+import { findAndTransformAlgoliaProducts } from '../modules/algolia/utils'
 
 export default async function productsChangedHandler({
   event,
   container
 }: SubscriberArgs<{ ids: string[] }>) {
-  const query = container.resolve(ContainerRegistrationKeys.QUERY)
   const algolia = container.resolve<AlgoliaModuleService>(ALGOLIA_MODULE)
 
-  const { data: products } = await query.graph({
-    entity: 'product',
-    fields: [
-      '*',
-      'categories.name',
-      'collection.title ',
-      'tags.value',
-      'type.value',
-      'variants.*',
-      'brand.name'
-    ],
-    filters: {
-      id: event.data.ids
-    }
-  })
-
-  const productsToInsert = z.array(AlgoliaProductValidator).parse(products)
+  const productsToInsert = await findAndTransformAlgoliaProducts(
+    container,
+    event.data.ids
+  )
   await algolia.batchUpsert(IndexType.PRODUCT, productsToInsert)
 }
 
