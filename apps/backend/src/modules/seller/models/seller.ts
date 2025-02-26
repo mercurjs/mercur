216aef78f49cd0ca493f2a179be1ab703811dc7e
slusarczykmichal <85502538+slusarczykmichal@users.noreply.github.com>
2025-02-26 07:33:56 +0100

    feat: expand seller info (#91)
    * feat: expand seller info
    
    * oas, query config

--- apps/backend/src/modules/seller/models/seller.ts
+++ apps/backend/src/modules/seller/models/seller.ts
@@ -9,6 +9,11 @@ export const Seller = model.define('seller', {
   handle: model.text().unique(),
   description: model.text().searchable().nullable(),
   photo: model.text().nullable(),
+  address_line: model.text().nullable(),
+  city: model.text().nullable(),
+  postal_code: model.text().nullable(),
+  country_code: model.text().nullable(),
+  tax_id: model.text().nullable(),
   members: model.hasMany(() => Member),
   invites: model.hasMany(() => Invite)
 })
