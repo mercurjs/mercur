036f90904c16fc744ec762d8c486645e8361bdad
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-03-24 13:41:05 +0100

    add export and import products

---
+++ apps/backend/src/workflows/seller/steps/get-seller-products.ts
@@ -0,0 +1,57 @@
+import { ContainerRegistrationKeys } from '@medusajs/framework/utils'
+import { StepResponse, createStep } from '@medusajs/framework/workflows-sdk'
+
+import sellerProduct from '../../../links/seller-product'
+
+export const exportProductFields = [
+  'id',
+  'title',
+  'subtitle',
+  'status',
+  'external_id',
+  'description',
+  'handle',
+  'is_giftcard',
+  'discountable',
+  'thumbnail',
+  'collection_id',
+  'type_id',
+  'weight',
+  'length',
+  'height',
+  'width',
+  'hs_code',
+  'origin_country',
+  'mid_code',
+  'material',
+  'metadata',
+  'brand.name',
+  'type',
+  'collection',
+  'options.*',
+  'options.values',
+  'tags.*',
+  'images.*',
+  'variants.*',
+  'variants.prices',
+  'variants.prices.price_rules.value',
+  'variants.prices.price_rules.attribute',
+  'variants.options.*'
+]
+
+export const getSellerProductsStep = createStep(
+  'get-seller-products',
+  async (seller_id: string, { container }) => {
+    const query = container.resolve(ContainerRegistrationKeys.QUERY)
+
+    const { data: products } = await query.graph({
+      entity: sellerProduct.entryPoint,
+      fields: exportProductFields.map((field) => `product.${field}`),
+      filters: {
+        seller_id
+      }
+    })
+
+    return new StepResponse(products.map((rel) => rel.product))
+  }
+)
---
+++ apps/backend/src/workflows/seller/steps/validate-products-to-import.ts
@@ -0,0 +1,15 @@
+import { StepResponse, createStep } from '@medusajs/framework/workflows-sdk'
+
+import { VendorCreateProduct } from '../../../api/vendor/products/validators'
+
+export const validateProductsToImportStep = createStep(
+  'validate-products-to-import',
+  async (products: unknown[]) => {
+    const toCreate = products.map((product) => ({
+      ...VendorCreateProduct.parse(product),
+      status: 'draft'
+    }))
+
+    return new StepResponse(toCreate)
+  }
+)
