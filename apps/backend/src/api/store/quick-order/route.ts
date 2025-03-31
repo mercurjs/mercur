d3b81ac8d671f185fc184488dbaacc7fad4569f2
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-03-31 18:33:58 +0200

    fix: quick order

--- apps/backend/src/api/store/quick-order/route.ts
+++ apps/backend/src/api/store/quick-order/route.ts
@@ -1,72 +1,32 @@
-import { fakerPL as faker } from '@faker-js/faker'
+import { MedusaRequest, MedusaResponse, container } from '@medusajs/framework'
 
-import { MedusaRequest, MedusaResponse } from '@medusajs/framework'
-import { ContainerRegistrationKeys, Modules } from '@medusajs/framework/utils'
-import { createOrderWorkflow } from '@medusajs/medusa/core-flows'
-
-import sellerProduct from '../../../links/seller-product'
-import { SELLER_MODULE } from '../../../modules/seller'
+import { splitAndCompleteCartWorkflow } from '../../../workflows/cart/workflows'
+import { getFormattedOrderSetListWorkflow } from '../../../workflows/order-set/workflows'
+import { createCart, createPayments, createShippingOptions } from './utils'
 import { CreateQuickOrderType } from './validators'
 
 export const POST = async (
   req: MedusaRequest<CreateQuickOrderType>,
   res: MedusaResponse
 ) => {
-  const remoteLink = req.scope.resolve(ContainerRegistrationKeys.LINK)
-  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY)
-  const address = {
-    first_name: faker.person.firstName(),
-    last_name: faker.person.lastName(),
-    city: faker.location.city(),
-    postal_code: faker.location.zipCode(),
-    province: faker.location.state(),
-    address_1: faker.location.streetAddress(),
-    address_2: faker.location.secondaryAddress(),
-    country_code: 'PL'
-  }
+  const cart = await createCart(req.validatedBody, req.scope)
+  await createShippingOptions(cart.id, container)
+  await createPayments(cart.id, container)
 
-  const { result: order } = await createOrderWorkflow.run({
+  const { result } = await splitAndCompleteCartWorkflow.run({
     container: req.scope,
     input: {
-      email: faker.internet.email({
-        firstName: address.first_name,
-        lastName: address.last_name
-      }),
-      shipping_address: address,
-      billing_address: address,
-      items: req.validatedBody.items,
-      region_id: req.validatedBody.region_id
+      id: cart.id
     }
   })
 
   const {
-    data: [product]
-  } = await query.graph({
-    entity: 'product_variant',
-    fields: ['product_id'],
-    filters: {
-      id: req.validatedBody.items[0].variant_id
-    }
+    result: { data }
+  } = await getFormattedOrderSetListWorkflow(req.scope).run({
+    input: { filters: { id: result.id } }
   })
 
-  const {
-    data: [seller]
-  } = await query.graph({
-    entity: sellerProduct.entryPoint,
-    fields: ['seller_id'],
-    filters: {
-      product_id: product.product_id
-    }
+  res.json({
+    order_set: data[0]
   })
-
-  await remoteLink.create({
-    [SELLER_MODULE]: {
-      seller_id: seller.seller_id
-    },
-    [Modules.ORDER]: {
-      order_id: order.id
-    }
-  })
-
-  res.json({ order })
 }
