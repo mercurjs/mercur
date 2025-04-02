bbcf05fe8aab5070c36e5dece6b42ff72720ef23
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-04-02 09:08:40 +0200

    modify algolia upsert

--- apps/backend/src/modules/algolia/utils/algolia-product.ts
+++ apps/backend/src/modules/algolia/utils/algolia-product.ts
@@ -1,10 +1,36 @@
 import { z } from 'zod'
 
 import { MedusaContainer } from '@medusajs/framework'
-import { ContainerRegistrationKeys } from '@medusajs/framework/utils'
+import {
+  ContainerRegistrationKeys,
+  arrayDifference
+} from '@medusajs/framework/utils'
 
 import { getAvgRating } from '../../reviews/utils'
 import { AlgoliaProductValidator } from '../types'
+
+export async function filterProductsByStatus(
+  container: MedusaContainer,
+  ids: string[] = []
+) {
+  const query = container.resolve(ContainerRegistrationKeys.QUERY)
+
+  const { data: products } = await query.graph({
+    entity: 'product',
+    fields: ['id', 'status'],
+    filters: {
+      id: ids
+    }
+  })
+
+  const published = products.filter((p) => p.status === 'published')
+  const other = arrayDifference(products, published)
+
+  return {
+    published: published.map((p) => p.id),
+    other: other.map((p) => p.id)
+  }
+}
 
 export async function findAndTransformAlgoliaProducts(
   container: MedusaContainer,
@@ -26,7 +52,8 @@ export async function findAndTransformAlgoliaProducts(
       'variants.prices.*',
       'brand.name',
       'options.*',
-      'options.values.*'
+      'options.values.*',
+      'images.*'
     ],
     filters: ids.length
       ? {
