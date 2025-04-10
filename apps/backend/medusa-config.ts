a00eb3fece64d944eea4e82efa78483c9ad16c4a
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-04-10 15:22:24 +0200

    reenable taxcode service

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -23,7 +23,12 @@ module.exports = defineConfig({
     { resolve: './src/modules/requests' },
     { resolve: './src/modules/reviews' },
     { resolve: './src/modules/brand' },
-    { resolve: './src/modules/taxcode' },
+    {
+      resolve: './src/modules/taxcode',
+      options: {
+        apiKey: process.env.STRIPE_SECRET_API_KEY
+      }
+    },
     { resolve: './src/modules/commission' },
     {
       resolve: './src/modules/payout',
