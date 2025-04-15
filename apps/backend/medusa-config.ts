4c0e3ef6c8b7d91d0b67bd26405acd3db5cf6fbc
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-04-15 11:16:39 +0200

    add s3 storage

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -87,6 +87,25 @@ module.exports = defineConfig({
           }
         ]
       }
+    },
+    {
+      resolve: '@medusajs/medusa/file',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/medusa/file-s3',
+            id: 's3',
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT
+            }
+          }
+        ]
+      }
     }
   ]
 })
