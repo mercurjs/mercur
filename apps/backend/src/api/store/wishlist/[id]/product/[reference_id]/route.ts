f7a53bcfa4964e101468f4252f037340bc45d35d
MWesterRigby <maciej.wester@rigbyjs.com>
2025-04-28 08:47:48 +0200

    add oas comment to route

--- apps/backend/src/api/store/wishlist/[id]/product/[reference_id]/route.ts
+++ apps/backend/src/api/store/wishlist/[id]/product/[reference_id]/route.ts
@@ -2,6 +2,45 @@ import { deleteWishlistWorkflow } from '#/workflows/wishlist/workflows'
 
 import { AuthenticatedMedusaRequest, MedusaResponse } from '@medusajs/framework'
 
+/**
+ * @oas [delete] /store/wishlist/{id}/product/{reference_id}
+ * operationId: "StoreDeleteWishlist"
+ * summary: "Delete a wishlist entry"
+ * description: "Deletes a wishlist entry by its ID for the authenticated user."
+ * x-authenticated: true
+ * parameters:
+ *   - name: id
+ *     in: path
+ *     required: true
+ *     description: The ID of the wishlist entry to delete.
+ *     schema:
+ *       type: string
+ * responses:
+ *   "200":
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             object:
+ *               type: object
+ *               description: Id of the wishlsit nad reference id.
+ *             object:
+ *               type: string
+ *               description: The type of the object that was deleted.
+ *               example: wishlist
+ *             deleted:
+ *               type: boolean
+ *               description: Whether the wishlist entry was successfully deleted.
+ *               example: true
+ * tags:
+ *   - Wishlist
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ */
+
 export const DELETE = async (
   req: AuthenticatedMedusaRequest,
   res: MedusaResponse
