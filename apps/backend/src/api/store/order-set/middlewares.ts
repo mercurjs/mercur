f1246de6c33a1010e67bc581090b7030d924bee3
slusarczykmichal <85502538+slusarczykmichal@users.noreply.github.com>
2025-05-08 07:49:57 +0200

    feat: add GET order set endpoint (#203)
    * fix: import
    
    * feat: add GET order set endpoint

---
+++ apps/backend/src/api/store/order-set/middlewares.ts
@@ -0,0 +1,17 @@
+import { MiddlewareRoute, validateAndTransformQuery } from '@medusajs/framework'
+
+import { orderSetQueryConfig } from './query-config'
+import { StoreGetOrderSetParams } from './validators'
+
+export const storeOrderSetMiddlewares: MiddlewareRoute[] = [
+  {
+    method: ['GET'],
+    matcher: '/store/order-set/:id',
+    middlewares: [
+      validateAndTransformQuery(
+        StoreGetOrderSetParams,
+        orderSetQueryConfig.retrieve
+      )
+    ]
+  }
+]
