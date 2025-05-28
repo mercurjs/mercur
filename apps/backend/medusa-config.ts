3945bc82f98d945d898691a177976d122558a4cb
INFIxChatnify <jintawat.infi@gmail.com>
2025-05-29 01:38:35 +0700

    Update medusa-config.ts

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
