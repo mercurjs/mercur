e3a7de3981682f7af304d5863a1e3a77d8449bb3
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-05-29 09:27:39 +0200

    feat: invite seller to platform

---
+++ apps/backend/src/workflows/seller/steps/send-invitation-email.ts
@@ -0,0 +1,27 @@
+import { Modules } from '@medusajs/framework/utils'
+import { StepResponse, createStep } from '@medusajs/framework/workflows-sdk'
+
+import { ResendNotificationTemplates } from '../../../modules/resend/types/templates'
+import { CreateSellerInvitationDTO } from '../../../modules/seller/types'
+
+export const sendSellerInvitationEmailStep = createStep(
+  'send-seller-invitation-email',
+  async (input: CreateSellerInvitationDTO, { container }) => {
+    const service = container.resolve(Modules.NOTIFICATION)
+
+    const notification = await service.createNotifications({
+      channel: 'email',
+      to: input.email,
+      template: ResendNotificationTemplates.NEW_SELLER_INVITATION,
+      content: {
+        subject: `You've been invited to join Mercur`
+      },
+      data: {
+        data: {
+          url: input.registration_url
+        }
+      }
+    })
+    return new StepResponse(notification)
+  }
+)
