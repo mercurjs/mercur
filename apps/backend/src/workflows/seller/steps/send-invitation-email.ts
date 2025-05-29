e741f18fbfb75bb51e2caed4ebaa0bc6a5fde634
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-05-29 09:33:05 +0200

    fix: create notification error handler

--- apps/backend/src/workflows/seller/steps/send-invitation-email.ts
+++ apps/backend/src/workflows/seller/steps/send-invitation-email.ts
@@ -1,4 +1,8 @@
-import { Modules } from '@medusajs/framework/utils'
+import {
+  ContainerRegistrationKeys,
+  MedusaError,
+  Modules
+} from '@medusajs/framework/utils'
 import { StepResponse, createStep } from '@medusajs/framework/workflows-sdk'
 
 import { ResendNotificationTemplates } from '../../../modules/resend/types/templates'
@@ -8,20 +12,30 @@ export const sendSellerInvitationEmailStep = createStep(
   'send-seller-invitation-email',
   async (input: CreateSellerInvitationDTO, { container }) => {
     const service = container.resolve(Modules.NOTIFICATION)
+    const logger = container.resolve(ContainerRegistrationKeys.LOGGER)
 
-    const notification = await service.createNotifications({
-      channel: 'email',
-      to: input.email,
-      template: ResendNotificationTemplates.NEW_SELLER_INVITATION,
-      content: {
-        subject: `You've been invited to join Mercur`
-      },
-      data: {
+    try {
+      const notification = await service.createNotifications({
+        channel: 'email',
+        to: input.email,
+        template: ResendNotificationTemplates.NEW_SELLER_INVITATION,
+        content: {
+          subject: `You've been invited to join Mercur`
+        },
         data: {
-          url: input.registration_url
+          data: {
+            url: input.registration_url
+          }
         }
-      }
-    })
-    return new StepResponse(notification)
+      })
+
+      return new StepResponse(notification)
+    } catch (e) {
+      logger.error(e)
+      throw new MedusaError(
+        MedusaError.Types.UNEXPECTED_STATE,
+        'Notification provider failed!'
+      )
+    }
   }
 )
