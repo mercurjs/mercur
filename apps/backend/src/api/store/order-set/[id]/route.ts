de792c0e4f8722748c9f62244a38a209d065080e
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-06-11 14:04:21 +0200

    fix: ad d seller to orderset fields

--- apps/backend/src/api/store/order-set/[id]/route.ts
+++ apps/backend/src/api/store/order-set/[id]/route.ts
@@ -1,6 +1,7 @@
 import { MedusaRequest, MedusaResponse } from '@medusajs/framework'
 
 import { getFormattedOrderSetListWorkflow } from '../../../../workflows/order-set/workflows'
+import { defaultStoreRetrieveOrderSetFields } from '../query-config'
 
 export async function GET(
   req: MedusaRequest,
@@ -11,7 +12,7 @@ export async function GET(
   const {
     result: { data }
   } = await getFormattedOrderSetListWorkflow(req.scope).run({
-    input: { filters: { id } }
+    input: { filters: { id }, fields: defaultStoreRetrieveOrderSetFields }
   })
 
   res.json({
