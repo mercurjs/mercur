590f4a583fc76058b7a58b233943ab3f6d945ff3
Erastus Iilonga <erastus.iilonga@capricorn.com.na>
2025-08-29 10:02:12 +0200

    Initial setup

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -16,12 +16,74 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
     },
     cookieOptions: {
-
       secure: false, // allow HTTP
       sameSite: 'lax' // or "strict", if appropriate
     }
   },
   modules: [
+    {
+      resolve: '@medusajs/medusa/cache-redis',
+      options: {
+        redisUrl: process.env.REDIS_URL
+      }
+    },
+    {
+      resolve: '@medusajs/medusa/event-bus-redis',
+      options: {
+        redisUrl: process.env.REDIS_URL
+      }
+    },
+    {
+      resolve: '@medusajs/medusa/workflow-engine-redis',
+      options: {
+        redis: {
+          url: process.env.REDIS_URL
+        }
+      }
+    },
+    {
+      resolve: '@medusajs/medusa/workflow-engine-redis',
+      options: {
+        redis: {
+          url: process.env.REDIS_URL
+        }
+      }
+    },
+    // {
+    //   resolve: '@medusajs/medusa/analytics',
+    //   options: {
+    //     providers: [
+    //       {
+    //         resolve: '@medusajs/analytics-posthog',
+    //         id: 'posthog',
+    //         options: {
+    //           posthogEventsKey: process.env.POSTHOG_EVENTS_API_KEY,
+    //           posthogHost: process.env.POSTHOG_HOST
+    //         }
+    //       }
+    //     ]
+    //   }
+    // },
+    {
+      resolve: '@medusajs/medusa/file',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/medusa/file-s3',
+            id: 's3',
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT
+              // other options...
+            }
+          }
+        ]
+      }
+    },
     { resolve: '@mercurjs/seller' },
     { resolve: '@mercurjs/reviews' },
     { resolve: '@mercurjs/marketplace' },
