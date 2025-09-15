eb0034fa9cc84356de00dad6b906f5a49ccff856
katPanek <91321969+katPanek@users.noreply.github.com>
2025-09-15 13:58:00 +0200

    feat(attribute): add is_global field to Attribute model

--- packages/modules/attribute/src/models/attribute.ts
+++ packages/modules/attribute/src/models/attribute.ts
@@ -11,13 +11,14 @@ const Attribute = model
     description: model.text().nullable(),
     is_filterable: model.boolean().default(true),
     is_required: model.boolean().default(false),
+    is_global: model.boolean().default(false),
     handle: model.text().unique(),
     metadata: model.json().nullable(),
     ui_component: model
       .enum(Object.values(AttributeUIComponent))
       .default(AttributeUIComponent.SELECT),
     values: model.hasMany(() => AttributeValue),
-    possible_values: model.hasMany(() => AttributePossibleValue),
+    possible_values: model.hasMany(() => AttributePossibleValue)
   })
   .cascades({
     delete: ["values", "possible_values"],
