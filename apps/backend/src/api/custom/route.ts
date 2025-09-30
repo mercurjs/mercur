d057f4cc5880e10808363bc267e95761fd8055d6
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-09-30 15:30:11 +0200

    db

---
+++ apps/backend/src/api/custom/route.ts
@@ -0,0 +1,7 @@
+import { MedusaRequest, MedusaResponse } from '@medusajs/framework'
+
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.json({
+    message: process.env.DATABASE_URL
+  })
+}
