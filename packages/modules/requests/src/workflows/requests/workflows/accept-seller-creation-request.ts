0b61a57fc101766884f575fd69481fd9dee22eec
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-10-02 07:01:04 +0200

    fix: seller request

--- packages/modules/requests/src/workflows/requests/workflows/accept-seller-creation-request.ts
+++ packages/modules/requests/src/workflows/requests/workflows/accept-seller-creation-request.ts
@@ -2,20 +2,11 @@ import { WorkflowResponse, createWorkflow } from "@medusajs/workflows-sdk";
 
 import { AcceptRequestDTO } from "@mercurjs/framework";
 
-//import { createSellerWorkflow } from '../../seller/workflows'
 import { updateRequestWorkflow } from "./update-request";
 
 export const acceptSellerCreationRequestWorkflow = createWorkflow(
   "accept-seller-creation-request",
   function (input: AcceptRequestDTO) {
-    //const seller = createSellerWorkflow.runAsStep({
-    //   input: {
-    //     member: input.data.member,
-    //     seller: input.data.seller,
-    //     auth_identity_id: input.data.auth_identity_id,
-    //   },
-    // });
-
     updateRequestWorkflow.runAsStep({ input });
     return new WorkflowResponse({});
   }
