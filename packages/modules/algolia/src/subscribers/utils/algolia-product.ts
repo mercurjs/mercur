e6b4ee60aa721fbe598c5bb981c0f9bea436a3ee
itariv <damian.grzesiak.dev@gmail.com>
2025-10-30 15:29:02 +0100

    chore: deleted incorrect relation info request

--- packages/modules/algolia/src/subscribers/utils/algolia-product.ts
+++ packages/modules/algolia/src/subscribers/utils/algolia-product.ts
@@ -134,7 +134,6 @@ export async function findAndTransformAlgoliaProducts(
       'variants.options.prices.*',
       'variants.prices.*',
       'variants.calculated_price.*',
-      'brand.name',
       'options.*',
       'options.values.*',
       'images.*',
