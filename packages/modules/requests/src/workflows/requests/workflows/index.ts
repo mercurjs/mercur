a7561d59a77903c6461da865e24eb7e7e28787db
itariv <damian.grzesiak.dev@gmail.com>
2025-11-06 13:38:23 +0100

    chore: added ability to create request for updating product collection by vendor

--- packages/modules/requests/src/workflows/requests/workflows/index.ts
+++ packages/modules/requests/src/workflows/requests/workflows/index.ts
@@ -3,6 +3,7 @@ export * from "./create-seller-creation-request";
 export * from "./update-request";
 export * from "./accept-product-category-request";
 export * from "./accept-product-collection-request";
+export * from "./accept-product-collection-update-request";
 export * from "./accept-product-request";
 export * from "./accept-seller-creation-request";
 export * from "./accept-review-remove-request";
