70a767ae64a861be0ef410765df27e78fd30c8c1
Tomasz Buras <tomaszburas.dev@gmail.com>
2025-12-23 14:29:27 +0100

    refactor: replace inline dateFilterSchema with import from shared utilities

--- packages/modules/b2c-core/src/api/vendor/product-types/validators.ts
+++ packages/modules/b2c-core/src/api/vendor/product-types/validators.ts
@@ -1,31 +1,7 @@
 import { z } from "zod";
 
 import { createFindParams } from "@medusajs/medusa/api/utils/validators";
-
-const dateFilterSchema = z
-  .preprocess(
-    (val) => {
-      if (typeof val === "string") {
-        try {
-          return JSON.parse(val);
-        } catch {
-          return val;
-        }
-      }
-      return val;
-    },
-    z
-      .object({
-        $gte: z.string().optional(),
-        $lte: z.string().optional(),
-        $gt: z.string().optional(),
-        $lt: z.string().optional(),
-        $eq: z.string().optional(),
-        $ne: z.string().optional(),
-      })
-      .optional()
-  )
-  .optional();
+import { dateFilterSchema } from "../../../shared/infra/http/utils";
 
 export const VendorGetProductTypesParamsFields = z.object({
   q: z.string().optional(),
