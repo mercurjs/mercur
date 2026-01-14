6b7c151ecdcee66bc27d9b2807fb3ad928940f5c
itariv <damian.grzesiak.dev@gmail.com>
2026-01-14 09:59:51 +0100

    feat: implement seller product rule synchronization after product deletion and refactor related workflows

--- packages/modules/b2c-core/src/workflows/hooks/product-deleted.ts
+++ packages/modules/b2c-core/src/workflows/hooks/product-deleted.ts
@@ -1,11 +1,14 @@
-import { Modules } from '@medusajs/framework/utils'
-import { deleteProductsWorkflow } from '@medusajs/medusa/core-flows'
+import { Modules } from "@medusajs/framework/utils";
+import { deleteProductsWorkflow } from "@medusajs/medusa/core-flows";
 
-import { AlgoliaEvents } from '@mercurjs/framework'
+import { AlgoliaEvents } from "@mercurjs/framework";
+import { syncSellersPromotionsDefaultProductRuleValues } from "../promotions/utils/sync-seller-product-rule-values";
 
 deleteProductsWorkflow.hooks.productsDeleted(async ({ ids }, { container }) => {
+  await syncSellersPromotionsDefaultProductRuleValues(container, ids, "remove");
+
   await container.resolve(Modules.EVENT_BUS).emit({
     name: AlgoliaEvents.PRODUCTS_DELETED,
-    data: { ids }
-  })
-})
+    data: { ids },
+  });
+});
