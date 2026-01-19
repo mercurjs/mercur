2b165139cc282b2d57f619a125527ef626e80d2e
Jakub Sieradzki <j.sieradzki45@gmail.com>
2026-01-19 16:14:54 +0100

    refactor: update response structure in test route to include additional test field

--- packages/registry/src/block-1/api/hello/test/route.ts
+++ packages/registry/src/block-1/api/hello/test/route.ts
@@ -2,6 +2,7 @@ import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     res.json({
-        test: true
+        test: true,
+        test2: false,
     })
 }
