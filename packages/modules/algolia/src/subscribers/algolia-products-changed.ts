f267a8d723bb26b19e443d1af954bf4fa5919b53
milosz-w-rigby <milosz.wisniewski@rigbyjs.com>
2026-01-20 17:35:02 +0100

    Mm2 2096 b 2 c vendor added image on product view is broken 3 (#621)
    * algolia fix error "Cannot read properties of undefined (reading 'attribute')"
    
    * algolia synchronization additional requests

--- packages/modules/algolia/src/subscribers/algolia-products-changed.ts
+++ packages/modules/algolia/src/subscribers/algolia-products-changed.ts
@@ -1,34 +1,53 @@
-import { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
+import { SubscriberArgs, SubscriberConfig } from '@medusajs/framework'
+import { ContainerRegistrationKeys } from '@medusajs/framework/utils'
 
-import { ALGOLIA_MODULE, AlgoliaModuleService } from "../modules/algolia";
-import { AlgoliaEvents, IndexType } from "@mercurjs/framework";
+import { AlgoliaEvents, IndexType } from '@mercurjs/framework'
 
+import { ALGOLIA_MODULE, AlgoliaModuleService } from '../modules/algolia'
 import {
   filterProductsByStatus,
-  findAndTransformAlgoliaProducts,
-} from "../subscribers/utils";
+  findAndTransformAlgoliaProducts
+} from '../subscribers/utils'
 
 export default async function algoliaProductsChangedHandler({
   event,
-  container,
+  container
 }: SubscriberArgs<{ ids: string[] }>) {
-  const algolia = container.resolve<AlgoliaModuleService>(ALGOLIA_MODULE);
+  const logger = container.resolve(ContainerRegistrationKeys.LOGGER)
 
-  const { published, other } = await filterProductsByStatus(
-    container,
-    event.data.ids
-  );
+  try {
+    const algolia = container.resolve<AlgoliaModuleService>(ALGOLIA_MODULE)
 
-  const productsToInsert = published.length
-    ? await findAndTransformAlgoliaProducts(container, published)
-    : [];
+    const { published, other } = await filterProductsByStatus(
+      container,
+      event.data.ids
+    )
 
-  await algolia.batch(IndexType.PRODUCT, productsToInsert, other);
+    logger.debug(
+      `Algolia sync: Processing ${event.data.ids.length} products - ${published.length} to upsert, ${other.length} to delete`
+    )
+
+    const productsToInsert = published.length
+      ? await findAndTransformAlgoliaProducts(container, published)
+      : []
+
+    await algolia.batch(IndexType.PRODUCT, productsToInsert, other)
+
+    logger.debug(
+      `Algolia sync: Successfully synced ${productsToInsert.length} products`
+    )
+  } catch (error) {
+    logger.error(
+      `Algolia sync failed for products ${event.data.ids.join(', ')}:`,
+      error
+    )
+    throw error
+  }
 }
 
 export const config: SubscriberConfig = {
   event: AlgoliaEvents.PRODUCTS_CHANGED,
   context: {
-    subscriberId: "algolia-products-changed-handler",
-  },
-};
+    subscriberId: 'algolia-products-changed-handler'
+  }
+}
