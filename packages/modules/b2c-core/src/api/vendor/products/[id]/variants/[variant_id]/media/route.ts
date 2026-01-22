a7b7619899430c2e1f0c716ef3040f0415996627
tomaszdworniczak <tomdworniczak@gmail.com>
2026-01-22 14:38:06 +0100

    fix[mm2-2498]: fix linting in new endpoint

--- packages/modules/b2c-core/src/api/vendor/products/[id]/variants/[variant_id]/media/route.ts
+++ packages/modules/b2c-core/src/api/vendor/products/[id]/variants/[variant_id]/media/route.ts
@@ -8,16 +8,16 @@ export const POST = async (
   ) => {
     const variantId = req.params.variant_id
 
-  const { result } = await batchVariantImagesWorkflow(req.scope).run({
-    input: {
-      variant_id: variantId,
-      add: req.validatedBody.add,
-      remove: req.validatedBody.remove,
-    },
-  })
+    const { result } = await batchVariantImagesWorkflow(req.scope).run({
+      input: {
+        variant_id: variantId,
+        add: req.validatedBody.add,
+        remove: req.validatedBody.remove,
+      },
+    })
 
-  res.status(200).json({
-    added: result.added,
-    removed: result.removed,
-  })
+    res.status(200).json({
+      added: result.added,
+      removed: result.removed,
+    })
   }
