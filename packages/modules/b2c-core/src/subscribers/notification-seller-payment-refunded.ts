cdd1aad5c594d18b0c3085ef548a354df09ea667
slusarczykmichal <slusarczyk.michal@outlook.com>
2026-02-17 11:34:10 +0100

    feat(mm2-2649): add payment-refunded notification

--- packages/modules/b2c-core/src/subscribers/notification-seller-payment-refunded.ts
+++ packages/modules/b2c-core/src/subscribers/notification-seller-payment-refunded.ts
@@ -1,27 +1,27 @@
 import { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
 import {
   ContainerRegistrationKeys,
-  FulfillmentWorkflowEvents,
   Modules,
+  PaymentEvents,
 } from "@medusajs/framework/utils";
 
-export default async function sellerFulfillmentDeliveredHandler({
+export default async function sellerPaymentRefundedHandler({
   event,
   container,
 }: SubscriberArgs<{ id: string }>) {
   const notificationService = container.resolve(Modules.NOTIFICATION);
   const query = container.resolve(ContainerRegistrationKeys.QUERY);
 
-  const { data: [fulfillment] } = await query.graph({
-    entity: 'fulfillment',
+  const { data: [payment] } = await query.graph({
+    entity: 'payment',
     fields: ['order.id', 'order.seller.id'],
     filters: {
       id: event.data.id
     }
   })
 
-  const orderId = fulfillment?.order?.id;
-  const sellerId = fulfillment?.order?.seller?.id;
+  const orderId = payment?.order?.id;
+  const sellerId = payment?.order?.seller?.id;
 
   if (!orderId || !sellerId) {
     return;
@@ -31,7 +31,7 @@ export default async function sellerFulfillmentDeliveredHandler({
     {
       to: sellerId,
       channel: "seller_feed",
-      template: "seller_fulfillment_delivered_notification",
+      template: "seller_payment_refunded_notification",
       data: {
         order_id: orderId,
       },
@@ -40,8 +40,8 @@ export default async function sellerFulfillmentDeliveredHandler({
 }
 
 export const config: SubscriberConfig = {
-  event: FulfillmentWorkflowEvents.DELIVERY_CREATED,
+  event: PaymentEvents.REFUNDED,
   context: {
-    subscriberId: "seller-fulfillment-delivered-handler",
+    subscriberId: "seller-payment-refunded-handler",
   },
 };
