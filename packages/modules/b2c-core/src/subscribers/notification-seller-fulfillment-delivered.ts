c1654cdfd3d976233505f06f2de246e9cc4fb995
slusarczykmichal <slusarczyk.michal@outlook.com>
2026-02-17 11:09:17 +0100

    feat(mm2-2650): add fulfillment-delivered notification

---
+++ packages/modules/b2c-core/src/subscribers/notification-seller-fulfillment-delivered.ts
@@ -0,0 +1,47 @@
+import { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
+import {
+  ContainerRegistrationKeys,
+  FulfillmentWorkflowEvents,
+  Modules,
+} from "@medusajs/framework/utils";
+
+export default async function sellerFulfillmentDeliveredHandler({
+  event,
+  container,
+}: SubscriberArgs<{ id: string }>) {
+  const notificationService = container.resolve(Modules.NOTIFICATION);
+  const query = container.resolve(ContainerRegistrationKeys.QUERY);
+
+  const { data: [fulfillment] } = await query.graph({
+    entity: 'fulfillment',
+    fields: ['order.id', 'order.seller.id'],
+    filters: {
+      id: event.data.id
+    }
+  })
+
+  const orderId = fulfillment?.order?.id;
+  const sellerId = fulfillment?.order?.seller?.id;
+
+  if (!orderId || !sellerId) {
+    return;
+  }
+
+  await notificationService.createNotifications([
+    {
+      to: sellerId,
+      channel: "seller_feed",
+      template: "seller_fulfillment_delivered_notification",
+      data: {
+        order_id: orderId,
+      },
+    },
+  ]);
+}
+
+export const config: SubscriberConfig = {
+  event: FulfillmentWorkflowEvents.DELIVERY_CREATED,
+  context: {
+    subscriberId: "seller-new-order-handler",
+  },
+};
