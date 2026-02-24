e20300d2ede6354d4de65a88ffbbbfa7644243a9
Tomasz Buras <91786940+tomaszburas@users.noreply.github.com>
2026-02-24 12:32:55 +0100

    feat(MM2-2467): add order action eligibility and fulfillment logic (#699)
    * chore: add @mercurjs/algolia dependency to backend package.json
    
    * chore: bump up packages version, update changelog
    
    * fix: release 1.5.1
    
    * release: 1.5.2
    
    Signed-off-by: tomaszdworniczak <tomdworniczak@gmail.com>
    
    * chore: update date in changelog
    
    Signed-off-by: tomaszdworniczak <tomdworniczak@gmail.com>
    
    * Create ISSUE_TEMPLATE initiative.md (#690)
    
    Co-authored-by: Grzegorz Tomaka <34004942+gtomaka@users.noreply.github.com>
    
    * fix: move ISSUE_TEMPLATE to correct location
    
    Moved ISSUE_TEMPLATE folder from .github/workflows/ to .github/ directory
    
    * Update README.md
    
    * feat(MM2-2467): add order action eligibility and fulfillment logic (NEW)
    
    ---------
    
    Signed-off-by: tomaszdworniczak <tomdworniczak@gmail.com>
    Co-authored-by: Tomek <50457289+tomaszdworniczak@users.noreply.github.com>
    Co-authored-by: tomaszdworniczak <tomdworniczak@gmail.com>
    Co-authored-by: Viktor Holik <109461604+vholik@users.noreply.github.com>
    Co-authored-by: Grzegorz Tomaka <34004942+gtomaka@users.noreply.github.com>
    Co-authored-by: Grzegorz Tomaka <grzesiektomaka@gmail.com>

--- packages/modules/b2c-core/src/api/admin/middlewares.ts
+++ packages/modules/b2c-core/src/api/admin/middlewares.ts
@@ -1,15 +1,15 @@
-import { MiddlewareRoute } from "@medusajs/framework";
+import { MiddlewareRoute } from '@medusajs/framework';
 
-import { attributeMiddlewares } from "./attributes/middlewares";
-import { configurationMiddleware } from "./configuration/middlewares";
-import { adminCustomMiddlewares } from "./custom/middlewares";
-import { adminOrdersMiddlewares } from "./orders/middlewares";
-import { orderSetsMiddlewares } from "./order-sets/middlewares";
-import { adminProductsMiddlewares } from "./products/middlewares";
-import { sellerMiddlewares } from "./sellers/middlewares";
-import { adminReservationsMiddlewares } from "./reservations/middlewares";
-import { collectionsMiddlewares } from "./collections/middlewares";
-import { productCategoriesMiddlewares } from "./product-categories/middlewares";
+import { attributeMiddlewares } from './attributes/middlewares';
+import { collectionsMiddlewares } from './collections/middlewares';
+import { configurationMiddleware } from './configuration/middlewares';
+import { adminCustomMiddlewares } from './custom/middlewares';
+import { orderSetsMiddlewares } from './order-sets/middlewares';
+import { adminOrdersMiddlewares } from './orders/middlewares';
+import { productCategoriesMiddlewares } from './product-categories/middlewares';
+import { adminProductsMiddlewares } from './products/middlewares';
+import { adminReservationsMiddlewares } from './reservations/middlewares';
+import { sellerMiddlewares } from './sellers/middlewares';
 
 export const adminMiddlewares: MiddlewareRoute[] = [
   ...orderSetsMiddlewares,
@@ -21,5 +21,5 @@ export const adminMiddlewares: MiddlewareRoute[] = [
   ...adminOrdersMiddlewares,
   ...adminReservationsMiddlewares,
   ...collectionsMiddlewares,
-  ...productCategoriesMiddlewares,
+  ...productCategoriesMiddlewares
 ];
