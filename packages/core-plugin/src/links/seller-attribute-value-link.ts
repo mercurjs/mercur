c16328975c7b91b98d49ae6c2b616d9e84f39ede
Jakub Sieradzki <j.sieradzki45@gmail.com>
2026-03-26 11:46:24 +0100

    feat(core): add attribute and vendor-product-attribute modules with migrations

---
+++ packages/core-plugin/src/links/seller-attribute-value-link.ts
@@ -0,0 +1,9 @@
+import { defineLink } from "@medusajs/framework/utils"
+
+import AttributeModule from "../modules/attribute"
+import SellerModule from "../modules/seller"
+
+export default defineLink(SellerModule.linkable.seller, {
+  linkable: AttributeModule.linkable.attributeValue,
+  isList: true,
+})
