24712d0c7471d003a3c0114f708efa3038511094
Jakub Sieradzki <j.sieradzki45@gmail.com>
2026-04-28 13:41:15 +0200

    chore: release v2.1.0

--- packages/registry/src/requests/workflows/requests/workflows/reject-request.ts
+++ packages/registry/src/requests/workflows/requests/workflows/reject-request.ts
@@ -1,5 +1,5 @@
 import { createWorkflow, WorkflowResponse } from "@medusajs/framework/workflows-sdk"
-import { upsertCustomFieldsStep } from "@mercurjs/core-plugin/workflows"
+import { upsertCustomFieldsStep } from "@mercurjs/core/workflows"
 
 import { RequestStatus } from "../../../types"
 import { validateRequestStatusStep } from "../steps"
