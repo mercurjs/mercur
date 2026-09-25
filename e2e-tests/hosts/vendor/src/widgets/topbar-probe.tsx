d68630f0cd5c855a42b25a4080ca96b34eac5abd
vholik <1viktorholik@gmail.com>
2026-09-25 15:12:08 +0200

    test: drop topbar widget zone e2e journey

--- e2e-tests/hosts/vendor/src/widgets/topbar-probe.tsx
+++
@@ -1,9 +0,0 @@
-import { defineWidgetConfig } from "@mercurjs/dashboard-sdk"
-
-const TopbarProbe = () => {
-  return <span data-testid="topbar-widget-probe">topbar widget</span>
-}
-
-export const config = defineWidgetConfig({ zone: "topbar.after" })
-
-export default TopbarProbe
